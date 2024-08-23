7fc8d496bbc79e949f232b405424491dc7180842
Arslanr369 <Arslanriaz785@gmail.com>
2024-08-23 09:02:10 +0500

    update

--- Problems/three_sum.py
+++ Problems/three_sum.py
@@ -2,3 +2,12 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()  
         result = []
+
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            
+            left, right = i + 1, len(nums) - 1
+            
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
