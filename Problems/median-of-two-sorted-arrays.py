999371a994c781b2c7cb12e43a0261d893b983d5
Arslanr369 <Arslanriaz785@gmail.com>
2024-08-23 09:36:29 +0500

    update

--- Problems/median-of-two-sorted-arrays.py
+++ Problems/median-of-two-sorted-arrays.py
@@ -40,3 +40,13 @@ class Solution:
                     min_right = min(nums1[i], nums2[j])
                 
                 return (max_left + min_right) / 2.0
+
+
+# Approach:
+# Binary Search on Smaller Array: We will perform a binary search on the smaller array (nums1 or nums2), ensuring that the left half of the combined arrays is less than or equal to the right half.
+# Partitioning: We split both arrays into two parts such that:
+# The total number of elements on the left side of the partition is equal to the total number of elements on the right side.
+# The largest element on the left side of both arrays combined is less than or equal to the smallest element on the right side.
+# Median Calculation:
+# If the combined length of the arrays is odd, the median will be the maximum element from the left partition.
+#If the combined length is even, the median will be the average of the maximum element from the left partition and the minimum element from the right partition.
