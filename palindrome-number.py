4d38d22add8288ff1d39c0fce955ce66d5bee21b
Arslanr369 <Arslanriaz785@gmail.com>
2024-08-23 10:33:14 +0500

    update

--- palindrome-number.py
+++ palindrome-number.py
@@ -10,4 +10,8 @@ class Solution:
         
         return x == reversed_half or x == reversed_half // 10
 
-# 
+# Approach:
+
+Negative Number Check: If x is negative, return false.
+Edge Case for Last Digit Zero: If x ends with a 0 and x is not 0, return false.
+Reversing Half of the Number: Reverse the second half of the digits. If the reversed half equals the first half, then the number is a palindrome.
