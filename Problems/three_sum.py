f86042c57db563d50ef7bef1ec1fbee64033174e
Arslanr369 <Arslanriaz785@gmail.com>
2024-08-23 09:02:44 +0500

    Update

--- Problems/three_sum.py
+++ Problems/three_sum.py
@@ -11,3 +11,16 @@ class Solution:
             
             while left < right:
                 total = nums[i] + nums[left] + nums[right]
+                
+                if total == 0:
+                    result.append([nums[i], nums[left], nums[right]])
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
+                elif total < 0:
+                    left += 1
+                else:
+                    right -= 1
