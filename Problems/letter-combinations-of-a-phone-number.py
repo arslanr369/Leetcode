6556a018bd42566b8cf5b7e0671092ea11894469
Arslanr369 <Arslanriaz785@gmail.com>
2024-08-24 12:55:43 +0500

    update

--- Problems/letter-combinations-of-a-phone-number.py
+++ Problems/letter-combinations-of-a-phone-number.py
@@ -14,7 +14,6 @@ class Solution:
         
         # Helper function for backtracking
         def backtrack(index, current_combination):
-            # If the current combination 
             if index == len(digits):
                 combinations.append(current_combination)
                 return
