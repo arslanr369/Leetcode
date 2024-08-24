6b1f6271dbcea36454eb3a3838f19b17e9bdbd61
Arslanr369 <Arslanriaz785@gmail.com>
2024-08-24 12:56:25 +0500

    update

--- Problems/letter-combinations-of-a-phone-number.py
+++ Problems/letter-combinations-of-a-phone-number.py
@@ -3,7 +3,6 @@ class Solution:
         if not digits:
             return []
         
-        # Mapping of digits to corresponding letters
         phone_map = {
             "2": "abc", "3": "def", "4": "ghi", "5": "jkl", 
             "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"
