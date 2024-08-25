7e7af4068ee49a2951bf3dfab3ad93b0ffe5d175
Arslanr369 <Arslanriaz785@gmail.com>
2024-08-25 11:58:46 +0500

    Update valid-parentheses.py

--- Problems/valid-parentheses.py
+++ Problems/valid-parentheses.py
@@ -21,7 +21,7 @@ class Solution:
 # Explanation:
 
 # Stack: The stack stores opening brackets as we encounter them.
-Bracket Map: The bracket_map dictionary maps each closing bracket to its corresponding opening bracket. This allows us to quickly check for matches.
+# Bracket Map: The bracket_map dictionary maps each closing bracket to its corresponding opening bracket. This allows us to quickly check for matches.
 Iterate through s:
 If the character is a closing bracket, check if the top of the stack is the corresponding opening bracket.
 If it is, pop the stack; if not, return False (the string is invalid).
