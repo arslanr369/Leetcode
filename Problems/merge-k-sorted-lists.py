af2841cf977927f063e4fd2ee36a34e26fa38a43
Arslanr369 <Arslanriaz785@gmail.com>
2024-08-25 12:17:44 +0500

    Update merge-k-sorted-lists.py

--- Problems/merge-k-sorted-lists.py
+++ Problems/merge-k-sorted-lists.py
@@ -1,7 +1,6 @@
 from heapq import heappop, heappush
 from typing import List, Optional
 
-# Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
@@ -9,10 +8,8 @@ class ListNode:
 
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        # Min-Heap to keep track of the smallest elements
         min_heap = []
         
-        # Initialize the heap with the head nodes of all the non-empty lists
         for i, l in enumerate(lists):
             if l:  
                 heappush(min_heap, (l.val, i, l))
