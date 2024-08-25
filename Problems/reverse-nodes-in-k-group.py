e096ba680863bd7d9316f7736d67cfa1bfb6a267
Arslanr369 <Arslanriaz785@gmail.com>
2024-08-25 12:28:02 +0500

    Update reverse-nodes-in-k-group.py

--- Problems/reverse-nodes-in-k-group.py
+++ Problems/reverse-nodes-in-k-group.py
@@ -5,29 +5,24 @@ class ListNode:
 
 class Solution:
     def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
-        # Dummy node to help with edge cases
         dummy = ListNode(0)
         dummy.next = head
         prev_group = dummy
         
         while True:
-            # Check if there are at least k nodes left to reverse
             kth_node = self.get_kth_node(prev_group, k)
             if not kth_node:
                 break
             
-            # Reverse the k nodes
             next_group = kth_node.next
             prev, curr = kth_node.next, prev_group.next
             
-            # Reverse the current group
             while curr != next_group:
                 temp = curr.next
                 curr.next = prev
                 prev = curr
                 curr = temp
             
-            # Connect with the previous part of the list
             temp = prev_group.next
             prev_group.next = kth_node
             prev_group = temp
