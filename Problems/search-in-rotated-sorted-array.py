14fb6749a1908fe6f5e45299df90045406db92ff
Arslanr369 <Arslanriaz785@gmail.com>
2024-08-26 12:17:43 +0500

    Update search-in-rotated-sorted-array.py

--- Problems/search-in-rotated-sorted-array.py
+++ Problems/search-in-rotated-sorted-array.py
@@ -20,6 +20,6 @@ class Solution:
 # Initial Setup: We initialize two pointers, left and right, to the beginning and end of the array, respectively.
 # Binary Search Loop: The loop continues until the left pointer surpasses the right pointer.
 # Check Mid Element: If nums[mid] equals the target, return mid.
-#Left Half Sorted: If nums[left] <= nums[mid], it means the left half is sorted. We then check if the target is within that range to decide which half to search next.
-#Right Half Sorted: If nums[mid] <= nums[right], it means the right half is sorted. Similarly, we check if the target is within that range.
-#Return Value: If the target is not found, return -1.
+# Left Half Sorted: If nums[left] <= nums[mid], it means the left half is sorted. We then check if the target is within that range to decide which half to search next.
+# Right Half Sorted: If nums[mid] <= nums[right], it means the right half is sorted. Similarly, we check if the target is within that range.
+# Return Value: If the target is not found, return -1.
