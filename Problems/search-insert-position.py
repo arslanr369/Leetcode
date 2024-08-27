812e54650e73cb1e1abdde759972af56a147317f
Arslanr369 <Arslanriaz785@gmail.com>
2024-08-27 09:25:26 +0500

    Update search-insert-position.py

--- Problems/search-insert-position.py
+++ Problems/search-insert-position.py
@@ -9,11 +9,13 @@ class Solution:
                 l = mid + 1
         return l
 # Approach:
+
 # Binary Search:
 
 # Start with two pointers, left and right, representing the bounds of the search space.
 
 # Calculate the mid index and compare nums[mid] with the target.
+
 # If nums[mid] is equal to the target, return mid.
 # If nums[mid] is less than the target, narrow the search to the right half of the array.
 # If nums[mid] is greater than the target, narrow the search to the left half of the array.
