2b7cbc300bf29f22a535ef2f87a51362d6fd1566
Arslanr369 <Arslanriaz785@gmail.com>
2024-08-27 09:51:43 +0500

    Update combination-sum.py

--- Problems/combination-sum.py
+++ Problems/combination-sum.py
@@ -23,7 +23,7 @@ class Solution:
 
 # Backtracking:
 
-Start with an empty combination and a sum of 0.
+# Start with an empty combination and a sum of 0.
 For each number in the candidates list, try to add it to the current combination.
 If the sum exceeds the target, stop exploring that path (backtrack).
 If the sum equals the target, add the current combination to the result.
