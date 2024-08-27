c5bed1b4c0bb2972c786515c6be74be98008cd6b
Arslanr369 <Arslanriaz785@gmail.com>
2024-08-27 09:32:43 +0500

    Update valid-sudoku.py

--- Problems/valid-sudoku.py
+++ Problems/valid-sudoku.py
@@ -25,6 +25,7 @@ class Solution:
 # Approach:
 
 We will use three sets of data structures to keep track of the values we encounter in rows, columns, and sub-grids.
+
 For each filled cell, we'll check:
 If the value already exists in the corresponding row set.
 If the value already exists in the corresponding column set.
