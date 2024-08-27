620967b28d60c2346ea1c5394d8fc2cf5e9cb8a8
Arslanr369 <Arslanriaz785@gmail.com>
2024-08-27 09:52:43 +0500

    Update combination-sum.py

--- Problems/combination-sum.py
+++ Problems/combination-sum.py
@@ -26,12 +26,14 @@ class Solution:
 # Start with an empty combination and a sum of 0.
 # For each number in the candidates list, try to add it to the current combination.
 # If the sum exceeds the target, stop exploring that path (backtrack).
-If the sum equals the target, add the current combination to the result.
-Recur by trying to add the current number again (since the number can be used multiple times).
-After exploring all possibilities, backtrack by removing the last added number and try the next number.
-Sorting:
+# If the sum equals the target, add the current combination to the result.
+# Recur by trying to add the current number again (since the number can be used multiple times).
+# After exploring all possibilities, backtrack by removing the last added number and try the next number.
 
-Sorting the candidates array can help to optimize the backtracking by stopping early if the sum exceeds the target.
-Avoiding Duplicates:
+# Sorting:
+
+# Sorting the candidates array can help to optimize the backtracking by stopping early if the sum exceeds the target.
+
+# Avoiding Duplicates:
 
 Ensure that each combination is unique by tracking the frequency of numbers in the combination.
