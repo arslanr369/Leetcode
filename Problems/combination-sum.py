e1949ef0b21d5051c31e0ea6e638762d31056c87
Arslanr369 <Arslanriaz785@gmail.com>
2024-08-27 09:50:07 +0500

    Update combination-sum.py

---
+++ Problems/combination-sum.py
@@ -0,0 +1,22 @@
+class Solution:
+    def combinationSum(self, candidates, target):
+        result = []
+        
+        def backtrack(remaining, combination, start):
+            if remaining == 0:
+                result.append(list(combination))
+                return
+            elif remaining < 0:
+                return
+            
+            for i in range(start, len(candidates)):
+                combination.append(candidates[i])
+                # Recursively call backtrack with the updated remaining sum
+                backtrack(remaining - candidates[i], combination, i)
+                # Backtrack, remove the last added element
+                combination.pop()
+        
+        candidates.sort()  # Sort to improve efficiency
+        backtrack(target, [], 0)
+        
+        return result
